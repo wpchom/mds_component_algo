b9ed7a0787304a148a70e19723027717a377c65e
pchom <pchom@hotmail.com>
2025-01-19 13:52:30 +0800

    add sha512/sha384

--- inc/algo_sha2.h
+++ inc/algo_sha2.h
@@ -15,7 +15,11 @@ typedef struct ALGO_SHA256_Context {
     uint8_t buff[0x40];
 } ALGO_SHA256_Context_t;
 
-typedef ALGO_SHA256_Context_t ALGO_SHA224_Context_t;
+typedef struct ALGO_SHA224_Context {
+    uint64_t total[0x02];
+    uint32_t state[0x08];
+    uint8_t buff[0x40];
+} ALGO_SHA224_Context_t;
 
 typedef struct ALGO_SHA512_Context {
     uint64_t total[0x02];
@@ -23,7 +27,11 @@ typedef struct ALGO_SHA512_Context {
     uint8_t buff[0x80];
 } ALGO_SHA512_Context_t;
 
-typedef ALGO_SHA512_Context_t ALGO_SHA384_Context_t;
+typedef struct ALGO_SHA384_Context {
+    uint64_t total[0x02];
+    uint64_t state[0x08];
+    uint8_t buff[0x80];
+} ALGO_SHA384_Context_t;
 
 #define ALGO_SHA224_DIGEST_SIZE (224 / 8)
 typedef struct ALGO_SHA224_Digest {
@@ -55,6 +63,16 @@ extern void ALGO_SHA224_Init(ALGO_SHA224_Context_t *ctx);
 extern void ALGO_SHA224_Update(ALGO_SHA224_Context_t *ctx, const uint8_t *data, size_t size);
 extern void ALGO_SHA224_Finish(ALGO_SHA224_Context_t *ctx, ALGO_SHA224_Digest_t *digest);
 extern void ALGO_SHA224_Calulate(const uint8_t *data, size_t len, ALGO_SHA224_Digest_t *digest);
+
+extern void ALGO_SHA512_Init(ALGO_SHA512_Context_t *ctx);
+extern void ALGO_SHA512_Update(ALGO_SHA512_Context_t *ctx, const uint8_t *data, size_t size);
+extern void ALGO_SHA512_Finish(ALGO_SHA512_Context_t *ctx, ALGO_SHA512_Digest_t *digest);
+extern void ALGO_SHA512_Calulate(const uint8_t *data, size_t len, ALGO_SHA512_Digest_t *digest);
+
+extern void ALGO_SHA384_Init(ALGO_SHA384_Context_t *ctx);
+extern void ALGO_SHA384_Update(ALGO_SHA384_Context_t *ctx, const uint8_t *data, size_t size);
+extern void ALGO_SHA384_Finish(ALGO_SHA384_Context_t *ctx, ALGO_SHA384_Digest_t *digest);
+extern void ALGO_SHA384_Calulate(const uint8_t *data, size_t len, ALGO_SHA384_Digest_t *digest);
 
 #ifdef __cpluplus
 }
